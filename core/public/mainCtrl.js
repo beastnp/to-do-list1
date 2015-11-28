b13b409c638e13763130241224fff49308ea6a32
Nick Peterson <peterson.nick2@gmail.com>
2015-11-28 15:56:04 -0700

    Nov28first

--- core/public/mainCtrl.js
+++ core/public/mainCtrl.js
@@ -1,6 +1,25 @@
 angular.module('toDoApp').controller('mainCtrl', function ($scope, mainService) {
 
-
-
+    $scope.getItems = function () {
+        mainService.getItems().then(function (response) {
+            $scope.items = response;
+        });
+    };
+    
+//    $scope.addItem = function() {
+//        mainService.addNewItem($scope.newItem).then(function (response) {
+//            $scope.getItems();
+//        });
+//        $scope.newItem = {};
+//    };
+    
+    $scope.addNewItem = function() {
+        mainService.addNewItem($scope.newItem).then(function(response) {
+            $scope.getItems();
+        });
+        $scope.newItem = {};
+    };
+    
+    $scope.getItems();
 
 })
--- core/public/mainService.js
+++ core/public/mainService.js
@@ -1,5 +1,31 @@
 angular.module('toDoApp').service('mainService', function ($http) {
 
+    this.getItems = function () {
+        return $http.get('/api/lists').then(function (response) {
+            return response.data;
+        });
+    };
 
+//    this.addNewItem = function (item) {
+//        return $http.post('/api/lists', item).then(function (response) {
+//            return response;
+//        });
+//    };
+    
+    this.addNewItem = function (item) {
+        return $http.post('/api/lists', item).then(function (response) {
+            return response;
+        });
+    };
 
-})
+    this.updateItem = function (item) {
+        var endpoint = '/api/lists/' + item._id;
+        return $http.put(endpoint, item);
+    };
+
+    this.updateItem = function (item) {
+        var endpoint = '/api/lists/' + item._id;
+        return $http.delete(endpoint);
+    };
+
+});
